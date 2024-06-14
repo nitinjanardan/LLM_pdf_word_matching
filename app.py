464062ebb327f0b6d81cbd67f8ae0f59e5e965ab
= <=>
2024-06-15 00:47:32 +0530

    build a function that extract vlaues from pdf and call llm

--- app.py
+++ app.py
@@ -5,22 +5,22 @@ import file_upload as fp
 st.title("PDF word Matcher")
 form = st.form("Basic")
 file_upload = form.file_uploader("Upload PDF", type=["pdf"])
-question = form.text_area("Enter your Question")
+question = form.text_area(label ="Enter your Question",  placeholder="Please provide each questions in separate line")
 submit = form.form_submit_button("Upload")
     # vlaidating the form.
 if submit:
     if file_upload is None or question =="":
         alert = st.warning("Please upload a file and question 💀💀")
-        time.sleep(3)
-        alert.empty()
+        # time.sleep(3)
+        # alert.empty()
     else:
         # st.write(file_upload)
         pdf_read = fp.pdf_upload(file_upload)
-        with st.spinner("uploading........"):
-            time.sleep(3)
-        st.write(pdf_read)
-        placeholder = st.empty()
-        time.sleep(5)
-        placeholder.empty()
+        # with st.spinner("uploading........"):
+            # time.sleep(3)
+        # st.write(pdf_read)
+        # placeholder = st.empty()
+        # time.sleep(5)
+        # placeholder.empty()
         pdf_que = fp.pdf_and_question(pdf_read,question)
-        time.sleep(4)
+        # time.sleep(4)
--- file_upload.py
+++ file_upload.py
@@ -2,7 +2,17 @@ import streamlit as st
 from streamlit_pdf_viewer import pdf_viewer
 import PyPDF2
 import re
-def pdf_upload(file):
+import openai
+from openai import OpenAI
+import os 
+import json 
+open_ai_key  = "sk-proj-z8pRgOjNVVqsdVlA1fhVT3BlbkFJFJ1CHApNkYuCbiLdpg7n"
+# Load GPT-3.5 model
+# model = OpenAI()  # Adjust parameters as needed
+
+os.environ['OPENAI_API_KEY'] = open_ai_key
+openai.api_key = open_ai_key
+def pdf_upload(file) -> str:
     pdf_file = file.name
 
     pdf_reader = PyPDF2.PdfReader(file)
@@ -13,7 +23,25 @@ def pdf_upload(file):
     # Display the content
     return content
     
-def pdf_and_question(pdf_read,question):
-    st.write(pdf_read)
-    st.write(question)
+def pdf_and_question(pdf_read,question) -> str:
+     client = OpenAI()
+     pdf_read= pdf_read.lower()
+     question = question.lower()
+     ques = question.split("\n")
+     
+     qna_dict = dict()
+     for i in range(0,len(ques)):
+        response = client.chat.completions.create(model="gpt-3.5-turbo-0125",
+     messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": f'The following is the context followed by a question.\n{pdf_read}\n\nQuestion: {ques} \nPlease answer the question based on the context.'},
+        {"role": "user", "content": f'Output should be word to word match if the {ques} is a word to word match. If the answer is of low confidence then give the output as "Data Not Available".'}
+    
+    ])
+        res_message =response.choices[0].message.content
+        qna_dict.update({ques[i]:res_message})
+     json_qna = json.dumps(qna_dict)
+     st.write(json_qna)
+
+
 
